912e79f782ae6fc127f864679169689d9c1d430f
yuki <yuki@tamu.edu>
2023-01-28 14:01:08 -0600

    Bruh

--- scrape.js
+++ scrape.js
@@ -2,12 +2,12 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio')
-const url = 'https://www.retailmenot.com/?u=4R6M5JDZFNEJZP5W7NQ6WVAYX4';
+const url = 'https://simplycodes.com/feature/common-coupon-phrases';
     axios(url)
         .then(response => {
             const html = response.data;
             const $ = cheerio.load(html)
-            const salePrice = $('CopyCode__Code-sc-10h0ylz-2 cVSRcS').text()
+            const salePrice = $('.vam fw600 fs25 mr7').text()
             console.log(salePrice);
         })
         .catch(console.error);
