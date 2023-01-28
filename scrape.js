660ef453cdc5d0b4c0f7cd76e690b53d6c10c346
yuki <yuki@tamu.edu>
2023-01-28 15:40:28 -0600

    Branch check

--- scrape.js
+++ scrape.js
@@ -3,35 +3,9 @@ const cheerio = require("cheerio");
 
 // Simply Codes
 
-// const fetchCouponsSimplyCodes = async () => {
-//     try {
-//         const response = await axios.get('https://simplycodes.com/feature/common-coupon-phrases');
-
-//         const html = response.data;
-
-//         const $ = cheerio.load(html);
-
-//         const coupons = [];
-
-//         $('.dnone').each((_idx, el) => {
-//             const coupon = $(el).text() // type string
-//             // console.log(typeof coupon);
-//             const tempArr = coupon.split("\n");
-//             if (tempArr[1]) {     
-//                 coupons.push(tempArr[1].trim())
-//             }
-//         });
-//         return coupons;
-//     } catch (error) {
-//         throw error;
-//     }
-// };
-
-// fetchCouponsSimplyCodes().then((info) => console.log(info));
-
-const fetchCoupons = async () => {
+const fetchCouponsSimplyCodes = async () => {
     try {
-        const response = await axios.get('https://www.retailmenot.com/?u=AXLI4ZPI35C5XPZFZ3F4HDLGPA');
+        const response = await axios.get('https://simplycodes.com/feature/common-coupon-phrases');
 
         const html = response.data;
 
@@ -39,7 +13,7 @@ const fetchCoupons = async () => {
 
         const coupons = [];
 
-        $('.RedemptionModalContent__RedemptionBody-sc-1c3tlo0-3.jGMPPB').each((_idx, el) => {
+        $('.dnone').each((_idx, el) => {
             const coupon = $(el).text() // type string
             // console.log(typeof coupon);
             const tempArr = coupon.split("\n");
@@ -53,4 +27,30 @@ const fetchCoupons = async () => {
     }
 };
 
-fetchCoupons().then((info) => console.log(info));
+fetchCouponsSimplyCodes().then((info) => console.log(info));
+
+// const fetchCoupons = async () => {
+//     try {
+//         const response = await axios.get('https://www.retailmenot.com/?u=AXLI4ZPI35C5XPZFZ3F4HDLGPA');
+
+//         const html = response.data;
+
+//         const $ = cheerio.load(html);
+
+//         const coupons = [];
+
+//         $('.RedemptionModalContent__RedemptionBody-sc-1c3tlo0-3.jGMPPB').each((_idx, el) => {
+//             const coupon = $(el).text() // type string
+//             // console.log(typeof coupon);
+//             const tempArr = coupon.split("\n");
+//             if (tempArr[1]) {     
+//                 coupons.push(tempArr[1].trim())
+//             }
+//         });
+//         return coupons;
+//     } catch (error) {
+//         throw error;
+//     }
+// };
+
+// fetchCoupons().then((info) => console.log(info));
