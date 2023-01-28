78874ccf6ce55499a5193e42e2dee22a569576a8
yuki <yuki@tamu.edu>
2023-01-28 13:38:55 -0600

    i dunno

---
+++ scrape.js
@@ -0,0 +1,13 @@
+// prob doesn't work with dynamic pages
+
+const axios = require('axios');
+const cheerio = require('cheerio')
+const url = 'https://www.retailmenot.com/?u=4R6M5JDZFNEJZP5W7NQ6WVAYX4';
+    axios(url)
+        .then(response => {
+            const html = response.data;
+            const $ = cheerio.load(html)
+            const salePrice = $('CopyCode__Code-sc-10h0ylz-2 cVSRcS').text()
+            console.log(salePrice);
+        })
+        .catch(console.error);
