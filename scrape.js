4d3e577a9eea8c587a30334d13d5ab76c9b0d9ab
yuki <yuki@tamu.edu>
2023-01-29 04:42:37 -0600

    better message

--- scrape.js
+++ scrape.js
@@ -12,10 +12,11 @@ const fetchCouponsSimplyCodes = async () => {
         const $ = cheerio.load(html);
 
         const coupons = [];
+        const savings = [];
 
         $('.dnone').each((_idx, el) => {
             const coupon = $(el).text() // type string
-            // console.log(typeof coupon);
+            // console.log(coupon);
             const tempArr = coupon.split("\n");
             if (tempArr[1]) {
                 coupons.push(tempArr[1].trim())
@@ -28,29 +29,3 @@ const fetchCouponsSimplyCodes = async () => {
 };
 
 fetchCouponsSimplyCodes().then((info) => console.log(info));
-
-// const fetchCoupons = async () => {
-//     try {
-//         const response = await axios.get('https://www.retailmenot.com/?u=AXLI4ZPI35C5XPZFZ3F4HDLGPA');
-
-//         const html = response.data;
-
-//         const $ = cheerio.load(html);
-
-//         const coupons = [];
-
-//         $('.RedemptionModalContent__RedemptionBody-sc-1c3tlo0-3.jGMPPB').each((_idx, el) => {
-//             const coupon = $(el).text() // type string
-//             // console.log(typeof coupon);
-//             const tempArr = coupon.split("\n");
-//             if (tempArr[1]) {     
-//                 coupons.push(tempArr[1].trim())
-//             }
-//         });
-//         return coupons;
-//     } catch (error) {
-//         throw error;
-//     }
-// };
-
-// fetchCoupons().then((info) => console.log(info));
